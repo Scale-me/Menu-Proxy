c6fffc134fc594b4a43d7a445a9605c7d2606bab
Wesley Luk <wesleyluk@Wesleys-MacBook-Pro.local>
2019-09-05 13:48:17 -0700

    Loads all service modules

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/:L', express.static('public'));
+app.listen(port, () => console.log('port ' + port + ' is listening...'));
