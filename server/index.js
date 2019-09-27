de945ff44dfefedabe07f52210e44cfce9341492
Daniel Kang <danieldonghoonkang@gmail.com>
2019-09-26 20:44:27 -0700

    Working condition

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/:id', express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => console.log('port ' + port + ' is listening...'));
