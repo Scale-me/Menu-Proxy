0a7b77f9884fdf284b5048186151414cd0856d83
Daniel Kang <danieldonghoonkang@gmail.com>
2019-09-26 19:45:17 -0700

    hi

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const newRelic = require('newrelic');
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/:L', express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, () => console.log('port ' + port + ' is listening...'));
