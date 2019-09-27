78d1d247358b52cf819371a3ce742cb5c4b5d16d
Daniel Kang <danieldonghoonkang@gmail.com>
2019-09-26 20:58:04 -0700

    key

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use('/loaderio-7b83a50c93df245d8b62c01ec0ef1bdb.txt', express.static(path.join(__dirname, '../public')));
+
 app.use('/:id', express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => console.log('port ' + port + ' is listening...'));
